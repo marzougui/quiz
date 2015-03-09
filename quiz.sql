5a2be344283c38b736c6f28f1a7d4d28fed76bc7
Safwen MARZOUGUI <Safwen MARZOUGUI>
2015-03-09 17:15:26 +0100

    dump Db

--- quiz.sql
+++ quiz.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `Category`
+--
+
+DROP TABLE IF EXISTS `Category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Category`
+--
+
+LOCK TABLES `Category` WRITE;
+/*!40000 ALTER TABLE `Category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Question`
 --
 
@@ -31,8 +55,9 @@ CREATE TABLE `Question` (
   `answer` int(11) DEFAULT NULL,
   `option4` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `comment` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `categoryId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +66,7 @@ CREATE TABLE `Question` (
 
 LOCK TABLES `Question` WRITE;
 /*!40000 ALTER TABLE `Question` DISABLE KEYS */;
-INSERT INTO `Question` VALUES (16,'A Foo Bar','option 1dsdsds','dfdfsdfsdfdqqqqqq','aaaaaaaaaa',9,'bbbbbbbbbbbb',NULL),(17,'A Foo Bar','option 1dsdsds','dfdfsdfsdfdqqqqqq','aaaaaaaaaa',9,'bbbbbbbbbbbb',NULL),(18,'Question description','option 1dsdsds','dfdfsdfsdfdqqqqqq','aaaaaaaaaa',9,'bbbbbbbbbbbb',NULL),(19,'Question description','Possible answer 1','Possible answer 2','Possible answer 3',1,'Possible answer 4','Comments to show'),(20,'صدفسد دف د ظف','صدفسد دف د ظف','صدفسد دف د ظف','صدفسد دف د ظف',1,'صدفسد دف د ظف','صدفسد دف د ظف'),(21,'Question description','Possible answer 1','Possible answer 2','Possible answer 3',1,'Possible answer 4','Comments to show');
+INSERT INTO `Question` VALUES (24,'How many parayers are obliged in the Day','8','5','2',2,'50','There are 5 obiligatory Prayers in the Day.',NULL),(25,'Qui est le succédeur du Khalifa \"Abou Bakr\"','Omar al Khattab','Othman Ibn Affan','Ali Ibn abi Talib',1,'7oudhayfa','Comments to show',NULL),(26,'كم عدد  الصلوات  المفروضة','5','1','4',1,'6','Comments to show',NULL),(44,'Hadith is','A verse from the Qur\'an','The first wife of the Prophet Muhammad Read more at http://www.beliefnet.com/Faiths/Islam/Quiz/How-Much-Do-You-Know-About-Islam.aspx#5Dy9jk3muPlMQoWR.99','A verified statement or saying of the Prophet Muhammad',3,'The name of an angel','Comments to show',NULL),(45,'All of the following are pillars of Islam, EXCEPT:','Fasting','Prayer','Prophethood',3,'Charity','Comments to show',NULL),(46,'The word \"Islam\" means:','Justice and peace','Peace achieved through submission to God','Peace achieved through submission to the Prophet\'s message',2,'The spread of peace around the world','Comments to show',NULL),(47,'Question description','Possible answer 1','Possible answer 2','Possible answer 3',1,'Possible answer 4','Comments to show',NULL);
 /*!40000 ALTER TABLE `Question` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-04 19:02:24
+-- Dump completed on 2015-03-09 17:15:17
